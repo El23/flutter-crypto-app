29c99b2a96f56f2ae5c1472fb40c74d491f643d8
Elen <elenneg23@gmail.com>
2021-01-28 22:01:37 +0200

    update authentication (add textFields+buttons)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_crypto/ui/authentication.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   runApp(MyApp());
--- lib/ui/authentication.dart
+++ lib/ui/authentication.dart
@@ -1,14 +1,69 @@
 import 'package:flutter/material.dart';
+
 class Authentication extends StatefulWidget {
   @override
   _AuthenticationState createState() => _AuthenticationState();
 }
 
 class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: Colors.blue),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: _emailField,
+              decoration: InputDecoration(
+                hintText: "...@mail.com",
+                hintStyle: TextStyle(color: Colors.white),
+                labelText: "Email",
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextFormField(
+              controller: _passwordField,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "your password",
+                hintStyle: TextStyle(color: Colors.white),
+                labelText: "Password",
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Register"),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Login"),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
